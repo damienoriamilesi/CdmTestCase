6547535e10c5c791cd56b275f395324c72189d0a
Damien <damien.oria@gmail.com>
2024-04-18 10:11:31 +0200

    init EF Core (#1)
    * init EF Core
    
    * Finalize tech test
    
    * Fix typo
    
    ---------
    
    Co-authored-by: dao <dao@cdm.smis.ch>

---
+++ SampleAPI/Class.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+class PizzaDbContext : DbContext
+{
+    public PizzaDbContext(DbContextOptions options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; } = null!;
+}
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
