03e86593e5ae5414109ef175dcf99c60d97387ef
Damien <damien.oria@gmail.com>
2024-04-18 10:11:31 +0200

    init EF Core (#1)
    * init EF Core
    
    * Finalize tech test
    
    * Fix typo
    
    ---------
    
    Co-authored-by: dao <dao@cdm.smis.ch>

--- SampleAPI/ToRefactor/AnotherSampleController.cs
+++ SampleAPI/ToRefactor/AnotherSampleController.cs
@@ -1,28 +1,22 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace SampleAPI.ToRefactor
+namespace SampleAPI.ToRefactor;
+
+[ApiController]
+[Route("[controller]")]
+public class AnotherSampleController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class AnotherSampleController : ControllerBase
+    private readonly IMediator _mediator;
+
+    public AnotherSampleController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public AnotherSampleController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
-        [HttpPost]
-        public IActionResult Create([FromBody] dynamic request)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public class CreateSampleObjectRequest
-        {
-        }
+    [HttpPost]
+    public IActionResult Create([FromBody] dynamic request)
+    {
+        throw new NotImplementedException();
     }
 }
